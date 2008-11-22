ed57cd87f46587a1446751f92ac91bf55556cdce
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2008-11-22 17:51:01 +0000

    Označkované české texty, základní stylování, náhodně přepínané pozadí, přepínání stránek v obsahu.

---
+++ switcher.js
@@ -0,0 +1,29 @@
+window.onload = function()
+{
+    var links = document.getElementsByTagName("a");
+    for (var i=0; i < links.length; i++)
+    {
+        if (links[i].href.indexOf("#") == -1)
+            continue;
+        links[i].onclick = function()
+        {
+            // Find the parent column div, so that we can
+            // collapse all the sections.
+            var parent = this;
+            while (parent.className.indexOf("column") == -1)
+                parent = parent.parentNode;
+
+            // Now collapse the sections.
+            var pages = parent.getElementsByTagName("div");
+            for (var j=0; j < pages.length; j++)
+                pages[j].style.display = "none";
+
+            // And finally open the right one.
+            var hash = this.href.indexOf("#");
+            var targetId = this.href.substring(hash+1, this.href.length);
+            var target = document.getElementById(targetId);
+            target.style.display = "block";
+            return false;
+        }
+    }
+}
